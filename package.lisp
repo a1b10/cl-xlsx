a4fb1c1a2d277428a75769568a4f4711f8da7b0e
Josephus Kim <gwangjin_kim@yahoo.com>
2018-09-15 16:44:35 +0200

    export all functions

--- package.lisp
+++ package.lisp
@@ -2,9 +2,25 @@
 
 (defpackage #:cl-xlsx
   (:use #:cl)
-  (:export #:list-sheets
-           #:read-sheet
-           #:as-matrix
-           #:as-alist
-           #:as-plist))
-
+  (:export #:list-entries
+	   #:get-entry
+	   #:with-open-xlsx
+	   #:once-flatten
+	   #:extract-sub-tags
+	   #:collect-extract-exprs
+	   #:select-tags-xlsx
+	   #:select-tags-xmlrep
+	   #:attr-val
+	   #:get-relationships
+	   #:get-unique-strings
+	   #:get-number-formats
+	   #:column-and-row
+	   #:excel-date
+	   #:list-sheets
+	   #:sheet-address
+	   #:begins-with?
+	   #:app-type
+	   #:process-table-cell
+	   #:process-table-row
+	   #:process-table-rows-ods
+	   #:read-ods))
